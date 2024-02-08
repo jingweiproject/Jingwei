3c7115e3f2a2e86f6c48dd211c5b896e77675f09
bizipoopoo <9813114+bizipoopoo@user.noreply.gitee.com>
2024-02-09 01:16:29 +0800

    jingweitianhai

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-# Sample Hardhat Project
+# 精卫
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+作者：@ezsyncxz
 
-Try running some of the following tasks:
+这是一个开拓思路的Demo项目，目的是想释放蓝筹NFT的流动性，不是新协议，而是作为区中心化的基础设施。
 
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
-```
+Jingwei合约提供了三种方法：createJFT,lockNFT,releaseNFT。
+
+通过createJFT创建NFT->FT的代币，通过lockNFT锁定NFT获得JFT，JFT是完全兼容Erc20协议，可以作为代币正常流通，通过releaseNFT销毁JFT获得NFT。
+
+通过事件和graph可以做成完全去中心化的产品，大致思路是网页上显示对应的仓库，仓库里锁定所有用户锁定的NFT，用户可以选择自己想要解锁的NFT，通过销毁指定数量的JFT解锁该NFT。
+
+以上只是我的个人想法，更多好玩的思路由大家去发掘。
+
+部署前请自行添加.evn文件。
+
+Jingwei:https://bscscan.com/address/0x81Ec6E444148b2c562f37FE52b155bC29Fd9b878#code
+JingweiFungibleToken:https://bscscan.com/address/0xFF7A49E94EEd587e15980dbFCf6587106630cBfB#code
+graph:https://thegraph.com/hosted-service/subgraph/jingweiproject/jingwei
+
+免责声明：改代码未经审核，本人不是solidity编码为主要工作，仅作为兴趣写的，因此无法保证代码安全，使用前请自行辨别，代码随意使用。
--- graph/jingwei/src/jingwei.ts
+++ graph/jingwei/src/jingwei.ts
@@ -5,53 +5,52 @@ import {
   NftLocked,
   NftReleased
 } from "../generated/Jingwei/Jingwei"
-import { ExampleEntity } from "../generated/schema"
+import { NonFungibleToken } from "../generated/schema"
+import { ONE_BI, ZERO_BI, getPlatform, getWareroom } from "./helpers"
+import { JingweiFungibleToken } from "../generated/JingweiFungibleToken/JingweiFungibleToken";
 
 export function handleJftCreated(event: JftCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+  let platform = getPlatform();
+  platform.allJfts = platform.allJfts.plus(ONE_BI);
+  platform.save();
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.token = event.params.token
-  entity.jft = event.params.jft
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allJfts(...)
-  // - contract.createJFT(...)
-  // - contract.getJft(...)
-  // - contract.getJftsLength(...)
+  let room = getWareroom(event.params.token);
+  room.nft = event.params.token;
+  room.jft = event.params.jft;
+  room.lockeNft = ZERO_BI;
+  room.time = event.block.timestamp;
+  room.save();
 }
 
-export function handleNftLocked(event: NftLocked): void {}
+export function handleNftLocked(event: NftLocked): void {
+  let nft = new NonFungibleToken(event.params.token.toHexString().concat(':').concat(event.params.tokenId.toString()));
+  nft.address = event.params.token;
+  nft.tokenId = event.params.tokenId;
+  nft.time = event.block.timestamp;
+  nft.save();
 
-export function handleNftReleased(event: NftReleased): void {}
+  let room = getWareroom(event.params.token);
+  room.lockeNft = room.lockeNft.plus(ONE_BI);
+  let arr = room.nftList;
+  arr.push(nft.id);
+  room.nftList = arr;
+  room.save();
+}
+
+export function handleNftReleased(event: NftReleased): void {
+  let room = getWareroom(event.params.token);
+  room.lockeNft = room.lockeNft.minus(ONE_BI);
+  let arr = room.nftList;
+  for (let i = ZERO_BI; i.lt(BigInt.fromI64(arr.length)); i = i.plus(ONE_BI)) {
+    
+  }
+  for (let i = 0; i<arr.length; i++) {
+    if (arr[i] === event.params.token.toHexString().concat(':').concat(event.params.tokenId.toString())) {
+      arr[i] = arr[arr.length - 1];
+      arr.pop();
+      room.nftList = arr;
+      break;
+    }
+  }
+  room.save();
+}
